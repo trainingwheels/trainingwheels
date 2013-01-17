98c46db5fda15615ce6cd1a7bb1f878b49ea1fc5
Mark Theunissen <mark.theunissen@gmail.com>
2013-01-17 15:36:44 -0600

    Working on vagrant

--- vagrant/tw-setup.sh
+++ vagrant/tw-setup.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-ls
-echo "hello, world"
+# Setup Ansible.
+sudo aptitude update
+sudo aptitude -y install git python-jinja2 python-yaml python-paramiko python-software-properties
+sudo add-apt-repository -y ppa:rquillo/ansible/ubuntu
+sudo aptitude update
+sudo aptitude install ansible
+echo "localhost" > /tmp/ansible-hosts
+sudo chown root: /tmp/ansible-hosts
+sudo mv /tmp/ansible-hosts /etc/ansible/hosts
+
+# Grab Training Wheels.
+sudo git clone https://github.com/fourkitchens/trainingwheels.git /tmp/trainingwheels
