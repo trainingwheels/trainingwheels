bdf15858f6f24f5e44efc27c6661de0d84eb4a81
Mark Theunissen <mark.theunissen@gmail.com>
2013-01-17 16:32:22 -0600

    Latest vagrant work

--- vagrant/README.md
+++ vagrant/README.md
@@ -2,9 +2,9 @@ Setup Vagrant Dev Environment
 -----------------------------
 
 1. Download and install Vagrant and VirtualBox as per the Vagrant "Getting started" page.
+2. Run `vagrant up`
 
-2. Install the Ansible Vagrant plugin:
+Why not use the Vagrant Ansible plugin?
+---------------------------------------
 
-    `vagrant gem install vagrant-ansible`
-
-Why are we not using the vagrant ansible plugin?  The problem is the setup of Ansible on the host Mac OSX machine. :<
+The problem is the setup of Ansible on the host Mac OSX machine. It's not straightforward as it requires Python modules be built. It's far simpler on Linux, but we must support OSX.
--- vagrant/tw-setup.sh
+++ vagrant/tw-setup.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
 
-# Setup Ansible.
+# Variables.
+TW_DIR='/tmp/trainingwheels'
+
+echo ''
+echo 'Setting up Ansible...'
 sudo aptitude update
 sudo aptitude -y install git python-jinja2 python-yaml python-paramiko python-software-properties
 sudo add-apt-repository -y ppa:rquillo/ansible/ubuntu
 sudo aptitude update
-sudo aptitude install ansible
+sudo aptitude -y install ansible
 echo "localhost" > /tmp/ansible-hosts
 sudo chown root: /tmp/ansible-hosts
 sudo mv /tmp/ansible-hosts /etc/ansible/hosts
 
-# Grab Training Wheels.
-sudo git clone https://github.com/fourkitchens/trainingwheels.git /tmp/trainingwheels
+echo ''
+echo 'Grabbing the Training Wheels source from Github...'
+sudo git clone --branch vagrant https://github.com/fourkitchens/trainingwheels.git $TW_DIR
+
+echo ''
+echo 'Copying the settings files for the controller and classroom playbooks...'
+cp $TW_DIR/vagrant/classroom-settings-vagrant.yml $TW_DIR/playbooks/classroom/settings.yml
+cp $TW_DIR/vagrant/controller-settings-vagrant.yml $TW_DIR/playbooks/controller/settings.yml
+
+echo ''
+echo 'Running classroom setup playbook...'
+cd $TW_DIR/playbooks/classroom
+ansible-playbook -c local --user=root setup.yml
+
+echo ''
+echo 'Running controller setup...'
+cd $TW_DIR/playbooks/controller
+ansible-playbook -c local --tags="common,dev" --user=root setup.yml
+
+echo ''
+echo 'Running sample data playbooks...'
+ansible-playbook -c local --user=root sample-data.yml
