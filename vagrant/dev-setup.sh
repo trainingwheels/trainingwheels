b19cec76cd05d104ff3c69451694a292ae317f95
Mark Theunissen <mark.theunissen@gmail.com>
2013-01-23 12:11:22 -0600

    Changes

--- vagrant/dev-setup.sh
+++ vagrant/dev-setup.sh
@@ -5,6 +5,6 @@ TW_DIR='/tmp/trainingwheels'
 
 echo ''
 echo '======================================================================='
-echo 'Setting up the Training Wheels developer packages...'
+echo 'Setting up the Training Wheels developer environment...'
 cd $TW_DIR/vagrant
 ansible-playbook -c local --user=root dev-setup-playbook.yml
