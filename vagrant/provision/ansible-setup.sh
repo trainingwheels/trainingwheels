3f536e631fe8c4d3e5ac82c334e31d74fa834a53
Elliott Foster <elliottf@codebrews.com>
2013-01-28 09:14:04 -0600

    mongodb_user ansible module.

--- vagrant/provision/ansible-setup.sh
+++ vagrant/provision/ansible-setup.sh
@@ -10,7 +10,8 @@ if [ `which ansible` ]; then
 fi
 
 sudo aptitude -q=2 update
-sudo aptitude -q=2 -y install git python-jinja2 python-yaml python-paramiko python-software-properties
+sudo aptitude -q=2 -y install build-essential git python-dev python-jinja2 python-yaml python-paramiko python-software-properties python-pip
+sudo pip install pymongo
 sudo add-apt-repository -y ppa:rquillo/ansible/ubuntu 2>&1
 sudo aptitude -q=2 update
 sudo aptitude -q=2 -y install ansible
