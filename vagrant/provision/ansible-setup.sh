5e84e3707efd9642fec09141ae4f48318180456b
Mark Theunissen <mark.theunissen@gmail.com>
2013-02-06 15:45:49 -0600

    Working on fixing up the install of Ansible

--- vagrant/provision/ansible-setup.sh
+++ vagrant/provision/ansible-setup.sh
@@ -12,11 +12,13 @@ fi
 aptitude -q=2 update
 aptitude -q=2 -y install build-essential git python-dev python-jinja2 python-yaml python-paramiko python-software-properties python-pip debhelper python-support cdbs
 
-git clone https://github.com/ansible/ansible.git /tmp/ansible
-cd /tmp/ansible
+mkdir /usr/local/src/ansible
+git clone https://github.com/ansible/ansible.git /usr/local/src/ansible
+cd /usr/local/src/ansible
 git checkout v0.9 2>&1
 make deb 2>&1
-cd /tmp
+cd /usr/local/src
 dpkg -i ansible_0.9_all.deb
+rm ansible_0.9_all.deb
 
 echo "localhost" > /etc/ansible/hosts
