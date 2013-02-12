0e85c7321e620140afa0ba0dafe3017953071005
Mark Theunissen <mark.theunissen@gmail.com>
2013-02-12 17:46:55 -0600

    Remove the valid strings check, it's now in the envir.

--- controller/src/TrainingWheels/Common/Util.php
+++ controller/src/TrainingWheels/Common/Util.php
@@ -6,28 +6,6 @@ use Exception;
 
 class Util {
   /**
-   * Ensure parameters are passed as actual strings.
-   */
-  public static function assertValidStrings($function, $params) {
-    Log::log($function, L_DEBUG);
-
-    if (is_string($params)) {
-      $params = array($params);
-    }
-
-    if (isset($params) && is_array($params) && count($params) > 0) {
-      foreach ($params as $param) {
-        if (!is_string($param) || strlen($param) < 2) {
-          throw new Exception("Invalid string parameter passed to $function function.");
-        }
-      }
-    }
-    else {
-      throw new Exception("No parameters passed to $function function.");
-    }
-  }
-
-  /**
    * Password generator.
    */
   function passwdGen() {
