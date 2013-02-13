e57605c8086df8f216de4cdf46e5f50bcb1e0f9c
Mark Theunissen <mark.theunissen@gmail.com>
2013-02-13 10:52:41 -0600

    Better logic in call method

--- controller/src/TrainingWheels/Environment/Environment.php
+++ controller/src/TrainingWheels/Environment/Environment.php
@@ -73,39 +73,33 @@ class Environment {
    * other plugin's methods quite easily.
    */
   public function __call($method, $args) {
-    if (isset($this->$method)) {
+    if (isset($this->$method) && is_callable($this->$method)) {
       $func = $this->$method;
-
-      if (is_callable($func)) {
-        // In debug mode, ensure that the arguments we're calling the function with
-        // are sane. Calling these environment functions with empty strings can cause
-        // damage and is almost certainly a bug. PHP isn't strict enough about
-        // enforcing certain things, so we have to do it ourselves.
-        if ($this->debug) {
-          $refl = new ReflectionFunction($func);
-          $namespace = $refl->getNamespaceName();
-          $full_name = $namespace . '::' . $method;
-          if ($refl->getNumberOfRequiredParameters() > count($args)) {
-            throw new Exception("Too few parameters passed to \"$full_name\"");
-          }
-          if (is_array($args) && count($args) > 0) {
-            foreach ($args as $arg) {
-              if (!is_string($arg) || strlen($arg) < 2) {
-                throw new Exception("Invalid string arg passed to \"$full_name\"");
-              }
+      // In debug mode, ensure that the arguments we're calling the function with
+      // are sane. Calling these environment functions with empty strings can cause
+      // damage and is almost certainly a bug. PHP isn't strict enough about
+      // enforcing certain things, so we have to do it ourselves.
+      if ($this->debug) {
+        $refl = new ReflectionFunction($func);
+        $namespace = $refl->getNamespaceName();
+        $full_name = $namespace . '::' . $method;
+        if ($refl->getNumberOfRequiredParameters() > count($args)) {
+          throw new Exception("Too few parameters passed to \"$full_name\"");
+        }
+        if (is_array($args) && count($args) > 0) {
+          foreach ($args as $arg) {
+            if (!is_string($arg) || strlen($arg) < 2) {
+              throw new Exception("Invalid string arg passed to \"$full_name\"");
             }
           }
-          Log::log($full_name, L_DEBUG);
         }
+        Log::log($full_name, L_DEBUG);
+      }
 
-        return call_user_func_array($func, $args);
-      }
-      else {
-        throw new BadMethodCallException("The method \"$method\" is not callable.");
-      }
+      return call_user_func_array($func, $args);
     }
     else {
-      throw new BadMethodCallException("The method \"$method\" does not exist.");
+      throw new BadMethodCallException("The method \"$method\" does not exist or is not callable.");
     }
   }
 }
